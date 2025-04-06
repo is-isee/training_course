57dfccd61d4416fd896b2dc9fb3d86032f760e05
iijima <h.iijima0202@gmail.com>
2025-04-06 17:36:24 +0900

    enabled katex and mermaid (closes #3)

---
+++ docs/javascripts/katex.js
@@ -0,0 +1,11 @@
+// https://squidfunk.github.io/mkdocs-material/reference/math/
+document$.subscribe(({ body }) => {
+  renderMathInElement(body, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\(", right: "\\)", display: false },
+      { left: "\\[", right: "\\]", display: true }
+    ],
+  })
+})
